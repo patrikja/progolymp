a4c953de277a2387027f9eaa26762605c014ddc6
Patrik Jansson <patrik.ja@gmail.com>
2015-11-24 23:20:57 +0100

    Some more experiments with hesthoppning

--- onlinekval/2015/hesthoppning.hs
+++ onlinekval/2015/hesthoppning.hs
@@ -5,7 +5,7 @@
 -- Hagen är omgiven av stängsel. Det är garanterat att indata alltid innehåller exakt två ’H’-celler.
 -- Output: Ditt program ska skriva ut ett ord på en rad - "JA" om hestarna kan mötas på någon cell och "NEJ" annars.
 
-import qualified Data.Map as Map
+import qualified Data.Map.Strict as Map
 import Data.Map (Map)
 
 data Ruta = Tom | Sten | Hest | Besökt
@@ -43,12 +43,14 @@ char2Ruta '.' = Tom
 char2Ruta '#' = Sten
 char2Ruta c   = error ("char2Ruta: otillåtet tecken: " ++ [c])
 
+ruta2Char :: Ruta -> Char
+ruta2Char Hest   = 'H'
+ruta2Char Tom    = '.'
+ruta2Char Sten   = '#'
+ruta2Char Besökt = '*'
+
 numrera :: [a] -> [(Int, a)]
 numrera = zip [1..]
-
-test1 = ["H.H","...",".#."]
-test2 = ["H#H","...",".#."]
-test3 = "H..":replicate 100 "..."++["H.."]
 
 ----------------------------------------------------------------
 -- Utgå från (en karta som visar alla platser nåbara efter n steg) och
@@ -62,24 +64,31 @@ solution = jaNej . fst . leta . byggKarta
 jaNej :: Bool -> String
 jaNej b = if b then "JA" else "NEJ"
 
-leta :: Karta -> (Bool, (Karta, [Pos]))
-leta karta = (check resultat, resultat)
-  where resultat = head (dropWhile (not.done) (iterate steg startPar))
-        startPar = (Map.adjust (const Tom) slutPos karta, [startPos])
+leta :: Karta -> (Bool, [Pos])
+leta karta = (check slutKarta, ps)
+  where (slutKarta, ps) = head (dropWhile (not.done) (iterate steg startPar))
+        (startPar, check) = prepare karta
+        done (k, ps) = null ps || check k
+
+prepare :: Karta -> ((Karta, [Pos]), Karta -> Bool)
+prepare karta = (startPar, check)
+  where startPar = (Map.adjust (const Tom) slutPos karta, [startPos])
         (startPos,Hest):(slutPos,Hest):_ = filter ((Hest==).snd) (Map.assocs karta)
-        done (k, ps)  = null ps || check (k, ps)
-        check (k, ps) = Map.lookup slutPos k == Just Besökt
+        done  (k, ps) = null ps || check k
+        check k = Just Besökt == Map.lookup slutPos k
+
 
 steg :: (Karta, [Pos]) -> (Karta, [Pos])
 steg (karta, intressant) = (nyKarta, nyaPos)
   where nyaPos = uniq [p | rk <- intressant, p <- hesthoppFrån karta rk]
         nyKarta = foldr (Map.adjust (const Besökt)) karta nyaPos
 
-uniq :: Ord a => [a] -> [a]
-uniq (x:xs) = uniq lt ++ x:uniq gt
+-- uniq :: Ord a => [a] -> [a]
+uniq :: [Pos] -> [Pos]
+uniq []     = []
+uniq (x:xs) = x : (uniq lt ++ uniq gt)
   where  lt = filter (<x) xs
          gt = filter (>x) xs
-uniq xs     = xs
 
 main :: IO ()
 main = do
@@ -89,3 +98,28 @@ main = do
       (m, _):_    = reads rest
   råKarta <- sequence (replicate n getLine)
   putStrLn (solution råKarta)
+
+----------------
+
+test1 = ["H.H","...",".#."]
+test2 = ["H#H","...",".#."]
+test3 = "H..":replicate 100 "..."++["H.."]
+
+test n = ('H':replicate (n-1) '.') : replicate (n-2) (replicate n '.') ++ [replicate (n-1) '.' ++ "H"]
+
+showKarta :: Karta -> String
+showKarta karta = unlines [[maybe ' ' ruta2Char (Map.lookup (r,k) karta) | r <- [1..mr]] | k <- [1..mk]]
+  where mr = maximum (map fst (Map.keys karta))
+        mk = maximum (map snd (Map.keys karta))
+
+poss2Karta :: [Pos] -> Karta
+poss2Karta ps = Map.fromList (zip ps (repeat Besökt))
+
+showPoss :: [Pos] -> String
+showPoss = showKarta . poss2Karta
+
+{-
+let (startPar, check) = prepare (byggKarta (test 200))
+let qs = iterate steg startPar
+let is = map snd qs
+-}
