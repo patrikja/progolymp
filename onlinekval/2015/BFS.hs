88fae3e0ee46ea8d2fb8a501e84275fa44d6827c
Patrik Jansson <patrik.ja@gmail.com>
2015-11-29 21:49:18 +0100

    Effektiv skridskor (breadth-first search)

--- onlinekval/2015/BFS.hs
+++ onlinekval/2015/BFS.hs
@@ -4,9 +4,28 @@ import Data.Set hiding (map, filter)
 data Sökproblem upos = Sökproblem {
   nästa   :: upos -> [upos],
   vinst   :: upos -> Bool
---  förlust :: upos -> Bool -- inbyggt i nästa
   }
 
+-- Bredden-först-sökning av ett sökproblem från en nod ger en lista
+-- med noder parat med avståndet från startnoden.
+
+bfs :: Ord a => Sökproblem a -> a -> [(Int, a)]
+bfs sp = bfs' sp empty . sing
+
+sing :: a -> [(Int, a)]
+sing x = [(0,x)]
+
+bfs' :: (Num i, Ord a) =>
+  Sökproblem a -> Set a -> [(i, a)] -> [(i, a)]
+bfs' sp visited [] = []
+bfs' sp visited ((level, node):lns)
+  | member node visited = bfs' sp visited lns
+  | otherwise = (level, node) : bfs' sp v' (lns ++ nexts) -- TODO use efficient queues
+      where nexts = map ((,) (level+1)) (nästa sp node)
+            v' = insert node visited
+
+{-
+-- felaktig lösning som kan ge en massa dubletter
 bfs :: Ord a => Sökproblem a -> a -> [(Int, a)]
 bfs sp = platta . numrera . bfs' sp empty
 
@@ -15,7 +34,7 @@ bfs' sp visited p | member p visited = []
                   | otherwise        = [p]:zipAppend (map (bfs' sp v') nexts)
   where nexts = nästa sp p
         v' = insert p visited
-
+-}
 lösning :: Ord upos => Sökproblem upos -> upos -> Int
 lösning sp = fst . head . filter (vinst sp . snd) . bfs sp
 
@@ -31,5 +50,16 @@ combine (x:xs) (y:ys) = (x++y) : combine xs ys
 numrera :: (Enum n, Num n) => [a] -> [(n,a)]
 numrera = zip [0..]
 
+-- Exempel: platta [(0,"ha"),(1,"du")] == [(0,'h'),(0,'a'),(1,'d'),(1,'u')]
 platta :: [(a, [b])] -> [(a, b)]
 platta = concatMap (\(i,xs)-> map ((,)i) xs)
+
+----------------
+
+-- Testa att söka "blint" åt alla håll i 2D efter diagonalen där rad == kolumn
+testGraf :: Sökproblem (Int, Int)
+testGraf = Sökproblem allaHåll samma
+  where allaHåll (r,k) = [(r-1,k),(r+1,k),(r,k-1),(r,k+1)]
+        samma (r,k) = r==k
+
+test = print (lösning testGraf (20,0))
--- onlinekval/2015/skridskor.hs
+++ onlinekval/2015/skridskor.hs
@@ -10,14 +10,15 @@
 -- Output: det minsta antal svängar Natalie behöver göra för att ta sig ut från isen på höger sida.
 
 import Data.Array
+import BFS
 
-type Rad = Int
-type Kolumn = Int
-type Pos = (Rad, Kolumn)
-type Hinder = Bool
-type Karta = (Pos, Array Pos Hinder)
+type Rad      = Int
+type Kolumn   = Int
+type Pos      = (Rad, Kolumn)
+type Hinder   = Bool
+type Karta    = (Pos, Array Pos Hinder)
 data Riktning = Ö | N | V | S
-  deriving (Eq, Show)
+  deriving (Eq, Ord, Show)
 
 ettSteg :: Riktning -> Pos -> Pos
 ettSteg Ö (r, k) = (r,   k+1)
@@ -25,42 +26,39 @@ ettSteg N (r, k) = (r+1, k  )
 ettSteg V (r, k) = (r,   k-1)
 ettSteg S (r, k) = (r-1, k  )
 
--- Viktiga operationer:
+type RPos = (Riktning, Pos)
+type UPos = Either Riktning RPos -- Left r betyder utanför kartan i riktning r
 
-data UtökadPos = Bra Pos | Klar | Farlig
-  deriving (Eq, Show)
+start :: UPos
+start = Right (Ö, (0, 0))
 
--- kollar om en utökad position är farlig (bryter mot villkoret)
-farlig :: UtökadPos -> Bool
-farlig = (Farlig==)
+byggSökproblem :: Karta -> Sökproblem UPos
+byggSökproblem karta = Sökproblem nästa vinst
+  where
+    -- Vi har en funktion som anger alla möjliga nästa positioner givet en startposition:
+    nästa :: UPos -> [UPos]
+    nästa (Left r)    = [] -- inga fler steg från utanför kartan
+    nästa (Right pos) = case gå karta pos of
+      Left r          -> [Left r]
+      Right (rikt, p) -> [Right (vänster rikt, p),
+                          Right (höger   rikt, p)]
 
-åk :: Karta -> Riktning -> Pos -> UtökadPos
-åk karta rikt pos | utanför karta nästa  = if rikt==Ö then Klar else Farlig
-                  | hinder  karta nästa  = Bra pos
-                  | otherwise            = åk karta rikt nästa
+    -- vi har en funktion som testar om en position är "vinnande"
+    vinst :: UPos -> Bool
+    vinst (Left Ö) = True
+    vinst _        = False
+
+gå :: Karta -> RPos -> UPos
+gå karta (rikt, pos) | utanför karta nästa = Left rikt
+                     | hinder  karta nästa = Right (rikt, pos)
+                     | otherwise           = gå karta (rikt, nästa)
   where nästa = ettSteg rikt pos
 
 utanför :: Karta -> Pos -> Bool
 utanför ((maxrad, maxkol), _) (r, k) =   r < 0 || r >= maxrad
                                      ||  k < 0 || k >= maxkol
-
 hinder :: Karta -> Pos -> Bool
 hinder (_, karta) p = karta!p
-
--- sök :: Karta -> Pos -> Rikt -> UtökadPos
-sök :: Karta -> Riktning -> Pos -> Tree Pos
-sök karta rikt pos = case åk karta rikt pos of
-  Farlig -> Fail
-  Klar   -> Done pos
-  Bra p  -> Try  pos (sök karta (vänster rikt) p)
-                     (sök karta (höger   rikt) p)
-
-data Tree a = Fail | Done a | Try a (Tree a) (Tree a)
-  deriving Show
--- Exempel:
-t1 :: Tree Int
-t1 = Try 1 (Try 2 (Done 3) Fail) (Try 4 (Try 5 (Done 6) (Done 7)) (Done 8))
-
 
 höger :: Riktning -> Riktning
 höger Ö = S
@@ -73,6 +71,20 @@ vänster S = Ö
 vänster Ö = N
 vänster N = V
 vänster V = S
+
+-- Nu har vi egentligen beskrivit en graf: noder är (utökade)
+-- positioner och närmaste grannar anges av funktionen nästa.
+-- Uppgiften blir att hitta kortaste vägen i grafen från en startnod
+-- till en vinst. Det problemet kan lösas med "bredden-först"-sökning
+-- som håller reda på vilka noder som redan besökts.
+
+körSökproblem :: Sökproblem UPos -> Int
+körSökproblem sp = lösning sp start   - 1
+
+solution :: Karta -> Int
+solution = körSökproblem . byggSökproblem
+
+----------------------------------------------------------------
 
 byggKarta :: (Pos, [[Char]]) -> Karta
 byggKarta ((maxr, maxk), text) =
@@ -87,47 +99,11 @@ main = do
   let rader, kolumner :: Int
       (rader, rest):_ = reads rOchK
       (kolumner, _):_ = reads rest
-  rows <- sequence (replicate rader getLine)
+  rows <- sequence (replicate rader (take kolumner `fmap` getLine))
   let karta = byggKarta ((rader,kolumner),rows)
-  let träd = sök karta Ö (0,0)
-  let svar = djupFörFörstaLösningen träd
+  let svar  = solution karta
   print svar
-  -- print (takeTree (svar+2) träd) -- for debugging
-
--- | Sök igenom trädet en "nivå" i taget
-bräddenFörst :: Tree a -> [[a]]
-bräddenFörst Fail        = []  : repeat []
-bräddenFörst (Done x)    = [x] : repeat []
-bräddenFörst (Try x l r) = []  : zipWith (++) lss rss
-  where lss = bräddenFörst l
-        rss = bräddenFörst r
-
-numrera :: (Num n, Enum n) => [a] -> [(n, a)]
-numrera = zip [0..]
-
-djupFörFörstaLösningen :: Eq a => Tree a -> Int
-djupFörFörstaLösningen = fst . head . dropWhile (null.snd) . numrera . bräddenFörst
--- djupFörFörstaLösningen = fst . head . dropWhile (null.snd) . numrera . bräddenFörst' []
-
--- Ett försöka att spara tid genom att undvika "loopar"
-bräddenFörst' :: Eq a => [a] -> Tree a -> [[a]]
-bräddenFörst' xs Fail        = []  : repeat []
-bräddenFörst' xs (Done x)    = [x] : repeat []
-bräddenFörst' xs (Try x l r) | x `elem` xs = []  : repeat [] -- Loop
-                             | otherwise   = []  : zipWith (++) lss rss
-  where lss = bräddenFörst' (x:xs) l
-        rss = bräddenFörst' (x:xs) r
-
-
-takeTree 0 t = Fail
-takeTree n Fail = Fail
-takeTree n (Done x) = Done x
-takeTree n (Try x l r) = Try x (takeTree (n-1) l)
-                               (takeTree (n-1) r)
-
-
--- Alternativ typ
--- import qualified Data.Map as Map
--- import Data.Map (Map)
--- type Karta = (Pos, Map Pos Hinder)
--- hinder (_, karta) p = Map.lookup p karta == Just True
+-- för felsökning:
+--  let sp    = byggSökproblem karta
+--  let lista = bfs sp start
+--  print (take 50 lista)
