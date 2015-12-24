387f77ea7fa63fa9e2f4a4b65897ccc7ec187f92
Patrik Jansson <patrik.ja@gmail.com>
2015-12-25 00:34:20 +0100

    Some notes on towers on the way to a faster algorithm

--- open/towers/Main.hs
+++ open/towers/Main.hs
@@ -4,7 +4,7 @@ module Main where
 import Data.List (sortBy, intersperse)
 import Data.Function (on)
 
-type PowerTower = (Integer, [Integer])
+type PowerTower = [Integer]
 
 main = do
   n <- readLn
@@ -13,17 +13,17 @@ main = do
   putStr (solution pts)
 
 solution :: [PowerTower] -> String
-solution = unlines . ("Case 1:":) . map showPT . sortOn fst
+solution = unlines . ("Case 1:":) . map showPT . sortBy comparePT
 
 -- This helper function is in Data.List now but was not in the version used by Kattis (ghc-7.6.3)
 sortOn :: Ord b => (a->b) -> [a] -> [a]
 sortOn f = sortBy (compare `on` f)
 
 showPT :: PowerTower -> String
-showPT (_, is) = concat (intersperse "^" (map show is))
+showPT = concat . intersperse "^" . map show
 
 line2PowerTower :: String -> PowerTower
-line2PowerTower s = (foldr (^) 1 is, is)
+line2PowerTower s = is
   where is = map read (splitBy '^' s)
 
 splitBy :: Eq a => a -> [a] -> [[a]]
@@ -31,3 +31,89 @@ splitBy sep xs | null first = []
                | otherwise  = first : splitBy sep (drop 1 rest)
   where
     (first, rest) = span (sep/=) xs
+
+eval :: [Integer] -> Integer
+eval = foldr (^) 1
+
+{-
+
+It is too expensive to fully compute the integers corresponding to the
+"PowerTowers":
+p 2 1 = 2   = 2
+p 2 2 = 2^2 = 4
+p 2 3 = 2^4 = 16
+p 2 4 = 2^16= 65536
+p 2 5 = 2^65536 = (a 1 followed by 65536 zeros in binary)
+p 2 6 = 2^(p 5) = already ridiculously large!
+and the task allows for numbers up to a maximum of
+p 100 100
+
+Thus the main question is how to efficiently compare two numbers given
+in the "PowerTower" format.
+
+-}
+
+comparePT x y = compPT (simplify x) (simplify y)
+
+-- Base cases (assuming the "1-simplification" has been done)
+compPT (i:is)  []      = GT
+compPT []      (j:js)  = LT
+compPT []      []      = EQ
+
+compPT (i:is) (j:js) = case compare i j of
+  EQ -> compPT is js -- a nice optimization
+  LT -> helper i j is js (compPT is js)
+  GT -> flipOrd (helper j i js is (compPT js is))
+     -- flipOrd (compPT (j:js) (i:is))
+
+flipOrd LT = GT
+flipOrd GT = LT
+flipOrd EQ = EQ
+
+-- precondition: helper i j is only called with i < j
+helper i j is js LT = LT
+helper i j is js EQ = LT
+helper i j is js GT = compare (eval (i:is)) (eval (j:js))
+  -- TODO: need to do some computation
+
+{-
+-- example calls:
+helper 2 15 [2,2] []  GT
+helper 3  9 [4]   [2] GT
+-}
+
+-- simplifications:
+
+simplify (1:is) = [] -- short for 1
+simplify (p:is) = p : simplify is
+simplify [] = []
+
+----------------
+
+{-
+Left to do: compute helper i j is js GT in a more clever way
+
+Facts: eval is >= 1 + eval js
+
+ln (i^x / j^y) = x*ln i - y*ln j >? 0
+
+x/y >? ln j/ln i
+
+ln j/ln i is never more than 7 but x/y is often huge
+
+so it may be useful to approximate the ratio x/y (even crudely).
+
+----------------
+
+(2^x)^y = 2^(x*y)
+
+x1 = 2log i
+y1 = 2log j
+
+2^(x1*2^(x2*2^(x3*...*2))) / 2^(y1*2^(y2*2^(y3*...*2))) =
+2^(x1*2^(x2*2^(x3*...*2)) - y1*2^(y2*2^(y3*...*2))) =
+
+2^(2log x1 + x2*2^(x3*...*2)) - 2^(2log y1 + y2*2^(y3*...*2))
+
+
+-}
